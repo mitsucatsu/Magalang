949c41219ac187483c01c92fa56670df9fe521eb
mitsucatsu <mmdelacruz156@gmail.com>
2023-11-09 14:19:09 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ['Rubik'],
+        Poppins: ['Poppins'],
+      },
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  plugins: [],
+}
